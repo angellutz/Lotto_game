3c25d8f6e563bf66a877c4a9ae583be6f8275a8a
andre1dragos <andrei_dragos@ymail.com>
2023-02-14 21:46:56 +0000

    solution for allowing user to select max 6 numbers

--- main.js
+++ main.js
@@ -6,24 +6,34 @@ const winningNumContainer = document.querySelector(
   "#winning-numbers-container"
 );
 
+//   declare a variable to update the number of clicks
+let click = 0;
+
 for (let i = 1; i < 51; i++) {
-  //create rounded containers for numbers
+  //create rounded container for number
   const ticketNumContainer = document.createElement("div");
   ticketNumContainer.setAttribute(
     "class",
     "rounded-num-container ticket-num-container"
   );
-  //create span element to store the numbers
+
+  //create span element to store the number
   const ticketNumber = document.createElement("span");
   ticketNumber.setAttribute("class", "fs-6 fw-bold");
   ticketNumber.textContent = i;
   ticketNumContainer.appendChild(ticketNumber);
   options.appendChild(ticketNumContainer);
 
-  // create event listener for targeting and displaying the numbers
+  // create event listener to display the users selected numbers
   ticketNumContainer.addEventListener("click", () => {
-    displayNumbers(choicesContainer, i);
-    ticketNumContainer.style.backgroundColor = "#A3A2DF";
+    click++;
+    if (click > 6) {
+      ticketNumContainer.removeEventListener;
+    } else {
+      displayNumbers(choicesContainer, i);
+      ticketNumContainer.style.backgroundColor = "var(--user-select)";
+      ticketNumContainer.style.color = "var(--primary-color)";
+    }
   });
 }
 
@@ -32,9 +42,9 @@ function displayNumbers(container, index) {
   const numberContainer = document.createElement("div");
   numberContainer.setAttribute(
     "class",
-    "rounded-num-container chosen-num-container border-white"
+    "rounded-num-container play-num-container border-white"
   );
-  numberContainer.style.backgroundColor = "#A3A2DF";
+  numberContainer.style.backgroundColor = "var(--user-select)";
   const number = document.createElement("span");
   number.setAttribute("class", "fs-2 fw-bold text-white");
   number.textContent = index;
